763edf893b863412c794220dcf01ce4761061c80
Yury <letelmimo@gmail.com>
2022-09-03 19:06:43 +0300

    Corrected task

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,26 +2,42 @@
 int a = 5, b = 7;
 
 int max1 = a;
+int min1 = a;
 if (a > max1) max1 = a;
+else min1 =a;   
 if (b > max1) max1 = b;
+else min1 = b;
 Console.Write("max=");
-Console.WriteLine(max1);
+Console.Write(max1);
+Console.Write("  min=");
+Console.WriteLine(min1);
+
 
 
 int aa = 2;
 int bb = 10;
 
 int max2 = aa;
+int min2 = aa;
 if (aa > max2) max2 = aa;
+else min2 =aa;
 if (bb > max2) max2 = bb;
+else min2 =bb;
 Console.Write("max=");
-Console.WriteLine(max2);
+Console.Write(max2);
+Console.Write("  min=");
+Console.WriteLine(min2);
 
 int aaa = -9;
 int bbb = -3;
 
 int max3 = aaa;
+int min3 = aaa;
 if (aaa > max3) max3 = aaa;
+else min3 =aaa;
 if (bbb > max3) max3 = bbb;
+else min2 =bbb;
 Console.Write("max=");
-Console.WriteLine(max3);
+Console.Write(max3);
+Console.Write("  min=");
+Console.WriteLine(min3);
