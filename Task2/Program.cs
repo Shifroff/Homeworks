d28d154c1e6529586128dbb44381ee583c980345
Yury <letelmimo@gmail.com>
2022-09-03 18:21:04 +0300

    Maximum in three lines

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Maximum in three lines of numbers
+int a = 5, b = 7;
+
+int max1 = a;
+if (a > max1) max1 = a;
+if (b > max1) max1 = b;
+Console.Write("max=");
+Console.WriteLine(max1);
+
+
+int aa = 2;
+int bb = 10;
+
+int max2 = aa;
+if (aa > max2) max2 = aa;
+if (bb > max2) max2 = bb;
+Console.Write("max=");
+Console.WriteLine(max2);
+
+int aaa = -9;
+int bbb = -3;
+
+int max3 = aaa;
+if (aaa > max3) max3 = aaa;
+if (bbb > max3) max3 = bbb;
+Console.Write("max=");
+Console.WriteLine(max3);
