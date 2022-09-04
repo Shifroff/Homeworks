77505d4a8479b82d3e4f8e9a9b5dbf0b91c993b8
Yury <letelmimo@gmail.com>
2022-09-04 07:37:28 +0300

    Show all even numbers

---
+++ Task8/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Show all even numbers
+        Console.Write("Enter the number:");
+        int Number = int.Parse(Console.ReadLine());
+        int i = 0;
+        int EvenNumber = 0;
+        if (Number % 2 == 0)
+            while (i < Number)
+            {
+                EvenNumber = i + 2;
+                Console.Write(EvenNumber);
+                Console.Write("  ");
+                i = i + 2;
+
+            }
+        else
+            while (i < Number - 1)
+            {
+                EvenNumber = i + 2;
+                Console.Write(EvenNumber);
+                Console.Write("  ");
+                i = i + 2;
+                               
+                 
+            }
+
+
+
+        Console.WriteLine("   Even Numbers");
+    }
+}
