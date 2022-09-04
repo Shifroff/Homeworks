580ee8c3a3590da8128e4a6f440964ea4ed257f3
Yury <letelmimo@gmail.com>
2022-09-04 06:24:01 +0300

    Parity check

---
+++ Task6/Program.cs
@@ -0,0 +1,6 @@
+﻿// Parity check
+Console.WriteLine("Enter the number:");
+int Number = Int32.Parse(Console.ReadLine());
+if (Number%2==0) Console.WriteLine("Number Even");
+  else Console.WriteLine("Number Odd");
+
