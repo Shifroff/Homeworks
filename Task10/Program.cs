398315b16bd6d0625695ad03de987b093bb23e17
Yury <letelmimo@gmail.com>
2022-09-08 13:38:34 +0300

    Second number

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n;
+        n = 3;
+        // Объявим одномерный массив Data с n-элементами 
+        int[] Data = new int[n];
+        int i = 0;
+        Console.WriteLine("Enter the three-degree number ");
+        while (i < n)
+        {
+            Data[i] = Convert.ToInt32(Console.ReadLine());
+            i++;
+        }
+        Console.WriteLine("Second number" + Data[1]);
+    }
+}
