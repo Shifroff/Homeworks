d74e22be05f89188ff799b8ec157194c21cc4ed0
Yury <letelmimo@gmail.com>
2022-09-09 13:38:22 +0300

    Unhandled exception. System.IndexOutOfRangeException: Index was outside the bounds of the array.    at System.String.get_Chars(Int32 index)    at Program.Main(String[] args) in C:\Users\user\Desktop\Study\HomeWorks\Task13\Program.cs:line 28

---
+++ Task13/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string userString = "";
+
+        //Цикл с постусловием
+        do
+        {
+            //Приглашение пользователю
+            Console.Write("Enter number: ");
+
+            /*
+            * Получение веденной с клавиатуры строки,
+            * введенная строка сохраняется в переменную userString
+            */
+            userString = Console.ReadLine();
+
+            //Вывод введенной строки
+            Console.WriteLine("Our number: " + userString);
+            if (userString.Length < 3)
+        {
+            Console.WriteLine("Element missing");
+
+        }
+        else
+        {
+          char chr = userString[3];
+          Console.WriteLine("",chr); 
+        }
+       
+            }    while (userString != "Exit");
+    }
+}
+       
