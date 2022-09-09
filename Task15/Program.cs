c68e0f1b026f69de61e1d9bc7d4fd3366884849d
Yury <letelmimo@gmail.com>
2022-09-09 15:04:43 +0300

    Weekdays enum

---
+++ Task15/Program.cs
@@ -0,0 +1,31 @@
+﻿
+public enum WeekDays 
+{
+  Monday = 1,
+  Tuesday =2,
+  Wednesday =3,
+  Thursday = 4,
+  Friday = 5,
+  Saturday = 6,
+  Sunday = 7,
+
+
+};
+  class Program
+{
+     public static void Main(string[] args)
+     {
+         Console.Write( "Enter number of the WeekDay:" );
+         var n = int.Parse(Console.ReadLine());
+         if (n < 6)
+         {
+           Console.WriteLine((WeekDays)((n) % Enum.GetValues(typeof(WeekDays)).Length)); 
+           Console.WriteLine("Working day");
+         }
+         else
+         {
+          Console.WriteLine((WeekDays)((n) % Enum.GetValues(typeof(WeekDays)).Length)); 
+          Console.WriteLine("Weekend");  
+         }
+     }
+}
