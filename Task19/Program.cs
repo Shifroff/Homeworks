e1ee79cf16914edb66bc1921fde190c9bdf10c06
Yury <letelmimo@gmail.com>
2022-09-13 11:31:33 +0300

    Palindrome (string)

---
+++ Task19/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter a number to Check Palindrome : ");
+string number = Console.ReadLine();
+string reverse = string.Empty;
+            
+for (int i = number.Length - 1; i >= 0; i--)
+{
+    reverse = reverse+number[i];
+}
+            
+if (number == reverse)
+{
+    Console.WriteLine($"{number} is Palindrome.");
+}
+else
+{
+        Console.WriteLine($"{number} is not Palindrome");
+}
