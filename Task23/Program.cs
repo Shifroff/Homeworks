1f5040c486e5eb7999109844040ed5df3b39a245
Yury <letelmimo@gmail.com>
2022-09-13 12:51:06 +0300

    Cube table

---
+++ Task23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+System.Console.WriteLine("Enter number");
+int number = int.Parse(Console.ReadLine());
+int length = number;
+for (int i = 1; i < length+1; i++)
+{ 
+  System.Console.WriteLine($"{i}x{i}x{i}={i*i*i}");  
+}
