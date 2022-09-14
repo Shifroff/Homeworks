bc13c28ed5713a05620afd459c2b6db236e64394
Yury <letelmimo@gmail.com>
2022-09-14 13:53:15 +0300

    Sum

---
+++ Task27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+System.Console.WriteLine("Enter the number");
+string S = Console.ReadLine();
+int sum = Summ(S);
+System.Console.WriteLine(sum);
+
+
+
+int Summ (string str)
+{
+    sum =0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        sum = sum + Convert.ToInt32(Convert.ToString(str[i])); 
+    }
+return sum;
+} 
