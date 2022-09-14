4935631e0f8f015e8d00411c596b7e69d7fed33a
Yury <letelmimo@gmail.com>
2022-09-14 13:16:15 +0300

    Exponentation

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+Console.WriteLine("Hello, World!");
+
+int num1 = Prompt("Enter number one = ");
+int num2 = Prompt("Enter number two = "); 
+double result = Math.Pow(num1, num2);
+Console.WriteLine($"{num1}^{num2} = {result}");
+
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    return Convert.ToInt32(value);
+}
